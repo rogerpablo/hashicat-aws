8560a4b29bd4c9ab8721f777292882345797d130
rogerpablo <56940550+rogerpablo@users.noreply.github.com>
2022-08-12 10:26:19 -0300

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,1 +1,14 @@
 
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
+  
+  
